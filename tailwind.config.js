2b37041b22d47c9d76af2d056c64b678c4a2128c
fajarfadillahh <fajaragustian979@gmail.com>
2022-01-01 09:55:22 +0700

    header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,26 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      title: "Playfair Display",
+      text: "DM Sans",
+    },
+    fontWeight: {
+      reguler: 400,
+      medium: 500,
+      bold: 700,
+    },
+    colors: {
+      transparent: "transparent",
+      primary: "#02073e",
+      title: "#0f2137",
+      grey: "#343d48",
+      red: "#db4a87",
+      white: "#fff",
+    },
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
