e39d5801bd2747d42353052dbb58069909c3cef0
fajarfadillahh <fajaragustian979@gmail.com>
2022-01-08 22:37:20 +0700

    scroll section active menu

--- src/js/main.js
+++ src/js/main.js
@@ -65,3 +65,27 @@ const toggleItem = (item) => {
     item.classList.add("accordion-open");
   }
 };
+
+// ===== SCROLL SECTION ACTIVE MENU =====
+const sections = document.querySelectorAll("section[id]");
+
+function sectionActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.header__nav a[href*= ${sectionId} ]`)
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(`.header__nav a[href*= ${sectionId} ]`)
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", sectionActive);
